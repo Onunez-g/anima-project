ee63318a4b631c748e259f2b8bac84be15825235
Onunez-g <nunez.orlando2@gmail.com>
2021-05-05 11:35:18 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar" >
+        <h2>Home</h2>
+        <h2>Anime</h2>
+        <h2>Episodes</h2>
+        <h2>Categories</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
